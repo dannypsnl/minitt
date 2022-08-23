604c76f389cdba0e88da41dbd91c3f9b9a4a5ca4
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-08-23 19:06:24 +0800

    full check, left readback
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@gmail.com>

--- check/infer.rkt
+++ check/infer.rkt
@@ -66,10 +66,74 @@
     [(E:Type l) (void)]
     [a (check k tele ctx a (Val:Type 0))]))
 
-(: check : Natural Telescope Context E Val -> Void)
-(define (check k telescope ctx e t)
-  (match e
-    [else (void)]))
+(: reduce-to-value : Case -> Val)
+(define (reduce-to-value case)
+  (define e (GCase-expr case))
+  (if (E? e)
+      (Eval e (GCase-context case))
+      e))
+
+(: readbackV : Nonnegative-Integer Val -> NE)
+(define (readbackV k v)
+  (match v))
+
+(: eq-normal-form : Nonnegative-Integer Val Val -> Void)
+(define (eq-normal-form k m1 m2)
+  (define e1 (readbackV k m1))
+  (define e2 (readbackV k m2))
+  (unless (equal? e1 e2)
+    (error 'eqnf "~a =/= ~a" e1 e2)))
+
+(: check : Nonnegative-Integer Telescope Context E Val -> Void)
+(define (check k tele ctx e t)
+  (match* (e t)
+    [((E:Lambda pat _ e) (Val:Pi t g))
+     (define gen (genV k))
+     (define new-ctx (update-context ctx pat t gen))
+     (check (add1 k) (GTelescope:UpVar tele pat gen) new-ctx e (Inst g gen))]
+    [((E:Pair e1 e2) (Val:Sigma t g))
+     (check k tele ctx e1 t)
+     (check k tele ctx e2 (Inst g (Eval e1 tele)))]
+    [((E:Constructor constructor-name e) (Val:Sum constructors))
+     (define c (hash-ref constructors constructor-name #f))
+     (if c
+         (check k tele ctx e (reduce-to-value c))
+         (error 'bad-constructor))]
+    [((E:Unit) (Val:One)) (void)]
+    [((E:One) (Val:Type 0)) (void)]
+    [((E:Pi (Typed pat a) b) (Val:Type 0))
+     (check k tele ctx a (Val:Type 0))
+     (define gen (genV k))
+     (define new-ctx (update-context ctx pat (Eval a tele) gen))
+     (check (add1 k) (GTelescope:UpVar tele pat gen) new-ctx b (Val:Type 0))]
+    [((E:Sigma (Typed pat a) b) (Val:Type 0))
+     (check k tele ctx (E:Pi (Typed pat a) b) (Val:Type 0))]
+    [((E:Sum constructors) (Val:Type 0))
+     (for ([(_ c) (in-hash constructors)])
+       (check k tele ctx c (Val:Type 0)))]
+    [((E:Declaration d e) t) (define new-ctx (checkD k tele ctx d))
+                             (check k (GTelescope:UpDec tele d) new-ctx e t)]
+    [(e t) (define t1 (checkI k tele ctx e))
+           (eq-normal-form k t t1)]))
+
+(: checkD : Nonnegative-Integer Telescope Context Decl -> Context)
+(define (checkD k tele ctx d)
+  (let ([a (Decl-signature d)]
+        [e (Decl-body d)]
+        [p (Decl-pattern d)])
+    (cond
+      [(Decl-recursive? d)
+       (define t (Eval a tele))
+       (define gen (genV k))
+       (define new-ctx (update-context ctx p t gen))
+       (check (add1 k) (GTelescope:UpVar tele p gen) new-ctx e t)
+       (define v (Eval e (GTelescope:UpDec tele d)))
+       (update-context ctx p t v)]
+      [else
+       (checkT k tele ctx a)
+       (define t (Eval a tele))
+       (check k tele ctx e t)
+       (update-context ctx p t (Eval e tele))])))
 
 (: checkI : Natural Telescope Context E -> Val)
 (define (checkI k telescope ctx e)
